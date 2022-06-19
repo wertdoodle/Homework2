1e0b1ecfac8510663fab474623c0468c4d73ee92
wertdoodle <sfgislam@gmail.com>
2022-06-19 10:24:10 -0400

    remove weird codding

---
+++ src/HW2/stingintreview.java
@@ -0,0 +1,12 @@
+package HW2;
+
+public class stingintreview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="3";
+		int converedString = Integer.valueOf(a);
+		System.out.println(9/converedString);
+	}
+
+}
